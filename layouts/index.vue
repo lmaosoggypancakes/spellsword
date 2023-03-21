019d1dbbbbfbf5fb15b341cf92159e46245d7744
lmaosoggypancakes <macerajosef@gmail.com>
2023-03-20 19:09:29 -0700

    feat: nav buttons

--- layouts/index.vue
+++ layouts/index.vue
@@ -3,9 +3,11 @@
     <nav
       class="py-4 px-16 border-b-2 border-secondary flex justify-center text-lg"
     >
-      <div class="w-11/12 grid grid-cols-2">
-        <div class="flex">
-          <p class="max-w-md">Spellsword</p>
+      <div class="w-11/12 grid grid-cols-2 items-center">
+        <p class="max-w-md">Spellsword</p>
+        <div class="flex justify-end space-x-2">
+          <button class="btn btn-secondary btn-outline">Login</button>
+          <button class="btn btn-secondary text-primary">Register</button>
         </div>
       </div>
     </nav>
