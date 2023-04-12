060d4d16a55704ff6d7b03453edbf94166d2a4f4
josef <macerajosef@gmail.com>
2023-04-11 21:26:33 -0700

    feat: book

--- components/index/book.vue
+++ components/index/book.vue
@@ -1,38 +1,16 @@
 <template>
   <ClientOnly>
     <TresCanvas clear-color="#0E0F19" shadows alpha>
+      <TresPerspectiveCamera :position="[15, 15, 15]" />
       <OrbitControls />
-      <TresScene>
-        <TresPerspectiveCamera
-          :position="[1, 2, 5]"
-          :fov="45"
-          :aspect="1"
-          :near="0.1"
-          :far="1000"
-        />
-        <TresMesh ref="boxRef" :scale="1" cast-shadow>
-          <TresBoxGeometry :args="[1, 1, 1]" />
-          <TresMeshNormalMaterial />
-        </TresMesh>
-      </TresScene>
+      <Suspense>
+        <GLTFModel path="/scene.gltf" cast-shadow ref="bookRef" />
+      </Suspense>
+      <TresPointLight :intensity="15" :position="[25, 25, 25]" />
     </TresCanvas>
   </ClientOnly>
 </template>
 
 <script setup lang="ts">
-import { ShallowRef, shallowRef } from "vue";
-
-import { useRenderLoop, TresInstance } from "@tresjs/core";
-import { OrbitControls } from "@tresjs/cientos";
-
-const boxRef: ShallowRef<TresInstance | null> = shallowRef(null);
-
-const { onLoop } = useRenderLoop();
-
-onLoop(({ delta, elapsed }) => {
-  if (boxRef.value) {
-    boxRef.value.rotation.y += delta;
-    boxRef.value.rotation.z = elapsed * 0.2;
-  }
-});
+import { OrbitControls, GLTFModel } from "@tresjs/cientos";
 </script>
