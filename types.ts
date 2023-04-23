b436130b74b196f979c7da0a4495f0ea6299ebc3
josef <macerajosef@gmail.com>
2023-04-23 14:04:23 -0700

    wip: game statistics util

--- types.ts
+++ types.ts
@@ -34,6 +34,8 @@ export interface PlayerGame {
 
 export interface GameStatistics {
   playerAccuracy: number;
+  playerPoints: number;
+  opponentPoints: number;
   opponentAccuracy: number;
   status: GameStatus;
 }
