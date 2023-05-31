cb56bfe138e16f37701ab1a0d0d659c161e7c367
lmaosoggypancakes <macerajosef@gmail.com>
2023-05-31 09:23:40 -0700

    fix: remove ugly book

--- components/index/hero.vue
+++ components/index/hero.vue
@@ -18,7 +18,6 @@
           </div>
         </div>
       </div>
-      <Book />
     </div>
   </div>
 </template>
--- plugins/tres.ts
+++
@@ -1,7 +0,0 @@
-import Book from "@/components/index/book.vue";
-import Tres from "@tresjs/core";
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.component("Book", Book);
-  nuxtApp.vueApp.use(Tres);
-});
