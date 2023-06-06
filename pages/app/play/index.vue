115e50feffef25f382b936632eba72c752866dfc
lmaosoggypancakes <macerajosef@gmail.com>
2023-06-06 00:10:36 -0700

    wip: better play icons

--- pages/app/play/index.vue
+++ pages/app/play/index.vue
@@ -1,24 +1,19 @@
 <template>
   <div
-    class="h-full w-full sm:px-8 lg:px-32 py-32 grid grid-cols-3 grid-flow-row col-auto gap-8 text-seasalt"
+    class="h-full w-full sm:px-8 lg:px-32 py-32 text-seasalt overflow-x-scroll"
   >
     <!-- <PlayButton primary class="outline-red-400 bg-red-400" :disabled="disabled">
       Play a friend
     </PlayButton> -->
-    <PlayButton
-      primary
-      class="bg-blue-200 outline-blue-200 flex flex-col justify-center items-center"
-      :disabled="disabled"
-      @click="matchmake()"
-    >
+    <PlayModePanel primary :disabled="disabled" @click="matchmake()">
       <MagnifyingGlassIcon class="w-16 h-16" />
       <span class="text-3xl mt-8">Find Match</span>
-    </PlayButton>
+    </PlayModePanel>
     <!-- <PlayButton class="bg-green-300 outline-green-300" :disabled="disabled">
       Play Computer
     </PlayButton> -->
     <!-- <PlayButton
-      primary
+      secondary
       class="bg-yellow-200 outline-yellow-200"
       :disabled="disabled"
     ></PlayButton> -->
@@ -31,10 +26,10 @@
 <script setup lang="ts">
 import { io } from "socket.io-client";
 import { MagnifyingGlassIcon } from "@heroicons/vue/24/outline";
-import PlayButton from "@/components/app/play/playButton.vue";
 import MatchmakingToast from "@/components/toasts/matchmaking.vue";
 import ConnectingToast from "@/components/toasts/connecting.vue";
 import ErrorToast from "@/components/toasts/error.vue";
+import PlayModePanel from "~/components/app/play/playModePanel.vue";
 
 definePageMeta({
   layout: "app",
