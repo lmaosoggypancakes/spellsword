b13d7d03b46847ca53cf0be6018e0fd6f663e6e3
lmaosoggypancakes <macerajosef@gmail.com>
2023-06-20 00:05:22 -0700

    wip: use wrap discord client in Arc<Mutex<>> for thread safety

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,53 +3,31 @@
   windows_subsystem = "windows"
 )]
 
-use discord_presence::{Client, Event};
-use std::{thread, time, cell::RefCell};
-use once_cell::{sync::Lazy};
-fn getClient() -> Client {
-  return Client::new(1117689291120836699);
-}
-static mut DRPC: Lazy<Client> = Lazy::new(getClient);
-fn main() {
-  unsafe {
-    
-    DRPC.on_ready(|_ctx| {
-      println!("Ready!");
-      if let Err(why) = DRPC.set_activity(|a| {
-        a.details("In Main Menu").assets(|ass| {
-          ass.large_image("logo")
-        }).timestamps(|time| {
-          time.start(time::SystemTime::now().duration_since(time::UNIX_EPOCH).expect("Error").as_secs())
-        })
-      }) {
-        println!("Failed to set presence: {}", why);
-      }
-    });
-    
-    DRPC.on_error(|ctx| {
-      eprint!("An error occurred: {}", ctx.event);
-  });
-  
-  
-  DRPC.start();
 
-}
+use declarative_discord_rich_presence::DeclarativeDiscordIpcClient;
+use declarative_discord_rich_presence::activity::{Activity, Timestamps};
+use tauri::{Manager, State};
+use std::{time};
+fn main() {
   tauri::Builder::default()
+  .setup(|app| Ok({
+    let client = DeclarativeDiscordIpcClient::new("1117689291120836699");
+
+    client.enable();
+   
+    let _ = client.set_activity(Activity::new()
+        .details("In Main Menu").timestamps(Timestamps::new().start(time::SystemTime::now().duration_since(time::UNIX_EPOCH).expect("Error").as_secs().try_into().unwrap()))
+    );
+    app.manage(client);
+  }))
   .run(tauri::generate_context!())
-  .expect("error while running tauri application");
+  .expect("error while running tauri application")
 }
 
 #[tauri::command]
-fn set_discord_activity(details: String) {
-  unsafe {
-
-    DRPC.set_activity(|a| {
-      a.details(details).assets(|ass| {
-        ass.large_image("logo")
-      })
-      .timestamps(|time| {
-        time.start(time::SystemTime::now().duration_since(time::UNIX_EPOCH).expect("Error").as_secs())
-      })
-    });
-  }
+fn set_activity(client: State<'_, DeclarativeDiscordIpcClient>) {
+    client.set_activity(
+      Activity::new()
+        .details("")
+    );
 }
