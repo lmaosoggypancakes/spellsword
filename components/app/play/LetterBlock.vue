82e9b70557aa38549e358dc3acc0565807b698f5
josef <macerajosef@gmail.com>
2023-06-28 08:51:02 -0700

    chore(LetterBlock.vue): adjust margin on LetterBlock component chore([id].vue): adjust alignment and justify content on ul element

--- components/app/play/LetterBlock.vue
+++ components/app/play/LetterBlock.vue
@@ -1,7 +1,7 @@
 <template>
   <li
     :id="String(letter.id)"
-    class="text-center lg:text-2xl py-2 border-2 bg-base-100 rounded-md transition-all cursor-pointer mb-4 px-4"
+    class="text-center lg:text-2xl py-2 border-2 bg-base-100 rounded-md transition-all cursor-pointer mb-4 px-4 mx-2"
     :class="{
       'grayscale blur-sm': letter.active && !queue,
       'hover:shadow-[inset_0_0_10px_rgba(169,255,203,1)] ': !letter.active,
