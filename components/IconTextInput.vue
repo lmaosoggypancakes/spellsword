7fae3f92e4cf2a99536146218c44d9042ebf7ae0
josef <macerajosef@gmail.com>
2023-07-03 13:20:46 -0700

    feat: better login page

--- components/IconTextInput.vue
+++ components/IconTextInput.vue
@@ -1,14 +1,36 @@
 <template>
-  <div class="inline-flex flex-row items-center">
+  <div class="inline-flex flex-row items-center w-full justify-center">
     <Icon
-      name="mdi:account"
+      :name="name"
       class="h-14 w-14 rounded-tl-md rounded-bl-md bg-secondary text-primary p-4"
     />
     <input
-      type="text"
-      placeholder="Username"
-      class="h-14 bg-primary border-2 border-secondary rounded-tr-md rounded-br-md outline-none pl-4"
-      v-model="username"
+      :type="type"
+      :placeholder="placeholder"
+      class="h-14 bg-primary border-2 border-secondary rounded-tr-md rounded-br-md outline-none pl-4 w-full"
+      @input="handleInput"
     />
   </div>
 </template>
+
+<script setup lang="ts">
+import buttonClick from "~/assets/button_click.mp3";
+import { Howl } from "howler";
+const sound = new Howl({
+  src: [buttonClick],
+});
+const props = defineProps<{
+  name: string;
+  type: "password" | "text";
+  modelValue: string;
+  placeholder?: string;
+}>();
+const emits = defineEmits<{
+  (e: "update:modelValue", val: string): void;
+}>();
+
+const handleInput = (e) => {
+  sound.play();
+  emits("update:modelValue", e.target.value);
+};
+</script>
--- pages/login.vue
+++ pages/login.vue
@@ -3,8 +3,8 @@
     <nuxt-img
       class="mx-auto my-auto lg:block hidden"
       src="/logo.png"
-      height="256"
-      width="256"
+      height="512"
+      width="512"
     />
     <div class="h-full flex items-center">
       <div class="h-[80%] w-full flex flex-col items-center space-y-8">
@@ -14,11 +14,52 @@
           height="128"
           width="128"
         />
-        <span class="text-5xl text-center">Spellsword</span>
-        <p class="text-center font-normal">
+        <span class="text-5xl md:text-7xl text-center" id="name"
+          >Spellsword</span
+        >
+        <p class="text-center font-normal max-w-md">
           An interactive word-based game designed to improve our command of the
           english language.
         </p>
+        <form class="space-y-8 md:max-w-md mx-16 md:mx-0" @submit.prevent>
+          <IconTextInput
+            name="mdi:account"
+            type="text"
+            placeholder="Username"
+            v-model="username"
+          />
+          <div>
+            <IconTextInput
+              name="mdi:key"
+              type="password"
+              placeholder="Password"
+              v-model="password"
+            />
+            <NuxtLink
+              href="/login/forgot"
+              class="text-xs float-right hover:underline"
+              >Forgot your password?</NuxtLink
+            >
+          </div>
+          <Button type="secondary" class="w-full">Login</Button>
+          <span class="text-sm text-center block">
+            New here?
+            <NuxtLink href="/register" class="link">
+              Create an Account
+            </NuxtLink>
+          </span>
+          <div class="hidden lg:divider">or login with</div>
+          <div class="grid lg:grid-cols-2 gap-4">
+            <button class="btn btn-outline btn-accent">
+              <Icon name="uil:google" class="w-8 h-8" />
+              <!-- <span>Log in with Google</span> -->
+            </button>
+            <button class="btn btn-outline btn-accent mb-8 md:mb-0">
+              <Icon name="uil:github" class="w-8 h-8" />
+              <!-- <span>Log in with GitHub</span> -->
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   </div>
@@ -30,4 +71,17 @@ definePageMeta({
 });
 
 const username = ref("");
+const password = ref("");
 </script>
+
+<style>
+#name {
+  /** TEXT GRADIENT */
+  color: #2dc7d3;
+  background-image: -webkit-linear-gradient(45deg, #2dc7d3 6%, #fcd0a1 82%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  text-fill-color: transparent;
+  -webkit-text-fill-color: transparent;
+}
+</style>
