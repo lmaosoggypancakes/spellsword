67b379abc6b6901b652dd1855649916f1d93caf7
lmaosoggypancakes <macerajosef@gmail.com>
2023-07-15 22:20:06 -0700

    fix(account.vue): fix incorrect href in Friends tab to point to the correct route

--- layouts/account.vue
+++ layouts/account.vue
@@ -39,7 +39,7 @@
           <NuxtLink
             active-class="tab-active"
             class="tab tab-sm lg:tab-lg tab-lifted"
-            href="/app/account/friends"
+            href="/app/"
             >Friends</NuxtLink
           >
         </div>
