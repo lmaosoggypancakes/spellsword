ec4e0231be5145435506a674d5a909b8b97e8842
lmaosoggypancakes <macerajosef@gmail.com>
2023-07-19 22:42:46 -0700

    wip: mobile navigation

--- components/LandingNavbar.vue
+++ components/LandingNavbar.vue
@@ -2,10 +2,35 @@
   <div
     class="navbar bg-base-100 z-10 place-self-center rounded-2xl mt-4 px-8 p-0"
   >
-    <div class="flex-1">
+    <div class="navbar-start lg:hidden">
+      <div class="dropdown">
+        <label tabindex="0" class="btn btn-info btn-outline">
+          <div class="swap swap-rotate">
+            <input type="checkbox" />
+            <Icon name="quill:hamburger" class="h-6 w-6 swap-off" />
+            <Icon name="quill:escape" class="h-6 w-6 swap-on" />
+          </div>
+        </label>
+        <ul
+          tabindex="0"
+          class="menu menu-sm dropdown-content mt-3 z-20 p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li><a>Item 1</a></li>
+          <li>
+            <a>Parent</a>
+            <ul class="p-2">
+              <li><a>Submenu 1</a></li>
+              <li><a>Submenu 2</a></li>
+            </ul>
+          </li>
+          <li><a>Item 3</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="flex-1 justify-end lg:justify-start">
       <NuxtLink href="/"><BrandLettering :small="true" /></NuxtLink>
     </div>
-    <div class="flex-none">
+    <div class="flex-none hidden lg:flex">
       <ul class="menu menu-horizontal">
         <div class="flex justify-end space-x-2">
           <LinkWithSound href="/login"
@@ -19,3 +44,7 @@
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const mobileDropdownToggled = ref(false);
+</script>
