cf22468cac94684d134a24688991ec2a2d2bf395
lmaosoggypancakes <macerajosef@gmail.com>
2023-08-06 22:02:28 -0700

    wip: header text scroll effect on landing page

--- components/index/Download.vue
+++ components/index/Download.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen bg-base-100 text-info grid grid-cols-5 font-sans">
     <ul
-      class="text-secondary font-normal space-y-16 grid grid-rows-5 grid-cols-2 justify-center items-center pl-4"
+      class="text-secondary font-normal grid grid-rows-5 grid-cols-2 justify-end place-items-center pl-4"
       v-motion-fade
     >
       <LetterBlock
@@ -14,13 +14,14 @@
     </ul>
     <div class="col-span-3">
       <h1
-        class="text-7xl text-info max-w-md mx-auto text-center text-shadow-sm shadow-info"
+        class="text-info mx-auto text-center text-shadow-sm shadow-info"
+        :style="`font-size: 3rem`"
       >
         The battlefield is yours
       </h1>
     </div>
     <ul
-      class="text-secondary font-normal space-y-16 grid grid-rows-5 grid-cols-2 justify-center items-center pr-4"
+      class="text-secondary font-normal grid grid-rows-5 grid-cols-2 justify-start place-items-center pr-4 text-3xl"
       v-motion-fade
     >
       <LetterBlock
@@ -34,6 +35,11 @@
 </template>
 
 <script setup lang="ts">
+import { useWindowScroll } from '@vueuse/core';
+
+const download: Ref<null | HTMLElement> = ref(null);
+const scroll = useWindowScroll();
+
 // create an object with letter_1 as the keys and randomly generated numbers as each valuer
 const letters_1 = ["A", "O", "C", "P", "L", "K", "R", "F", "I", "Q"].reduce(
   (acc, curr) => {
